a7b6dedc7e90238d7e195919a9291da718d62e8e
ianin <1033553441@qq.com>
2021-04-20 21:53:26 +0800

    json to model

---
+++ happy_flutter/lib/learning/dart/public_setting.dart
@@ -0,0 +1,6 @@
+import 'package:json_annotation/json_annotation.dart';
+
+
+//https://www.jianshu.com/p/976f02f4f933
+//https://bbs.huaweicloud.com/blogs/208076
+//https://www.jianshu.com/p/ddfdde57d9e0
